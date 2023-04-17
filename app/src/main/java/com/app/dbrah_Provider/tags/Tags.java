41450725e46d52bdb30e789a603ad660da0994cc
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2023-04-17 14:25:27 +0200

    ch

--- app/src/main/java/com/app/dbrah_Provider/tags/Tags.java
+++ app/src/main/java/com/app/dbrah_Provider/tags/Tags.java
@@ -2,7 +2,7 @@ package com.app.dbrah_Provider.tags;
 
 public class Tags {
 
-    public static String base_url = "https://rsuppliers.com/";
+    public static String base_url = "https://dbrahapp.com/";
     public static final int not_id = 3254;
     public static final String not_tag = "dbrah_Provider_not";
 
