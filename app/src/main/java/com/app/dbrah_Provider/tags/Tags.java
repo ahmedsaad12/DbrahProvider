e4dc53f73d2855197bd2379bb48369ccfc1dd4d8
arabDevCom <semicolonsales@gmail.com>
2023-05-29 15:12:59 +0300

    ch

--- app/src/main/java/com/app/dbrah_Provider/tags/Tags.java
+++ app/src/main/java/com/app/dbrah_Provider/tags/Tags.java
@@ -2,7 +2,7 @@ package com.app.dbrah_Provider.tags;
 
 public class Tags {
 
-    public static String base_url = "https://dbrahapp.com/";
+    public static String base_url = "https://dashboard.dbrahapp.com/";
     public static final int not_id = 3254;
     public static final String not_tag = "dbrah_Provider_not";
 
